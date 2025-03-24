9f5c81e1c677fe5bd67d46cbf4b90c7cc15ab33c
Ojasva Tomar <ojasvatomar47@gmail.com>
2025-03-24 15:42:23 +0530

    Tailwind Initialization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const App = () => {
+export default function App() {
   return (
-    <h1>hi hello world</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App

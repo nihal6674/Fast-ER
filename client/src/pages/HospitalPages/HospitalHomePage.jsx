74bb35093194b94c80c51bd4c5beed567af92c93
Ojasva Tomar <ojasvatomar47@gmail.com>
2025-03-24 16:31:03 +0530

    React router set-up

---
+++ client/src/pages/HospitalPages/HospitalHomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HospitalHomePage = () => {
+    return (
+        <div>HospitalHomePage</div>
+    )
+}
+
+export default HospitalHomePage
---
+++ client/src/pages/PatientPages/PatientHomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PatientHomePage = () => {
+    return (
+        <div>PatientHomePage</div>
+    )
+}
+
+export default PatientHomePage
